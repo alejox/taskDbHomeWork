dff663ac87c2de8abd6fafeb77ed79695e3123e6
Alejandro Grajales <alejoxdrums@gmail.com>
2022-06-29 14:56:00 -0500

    FirstCommit

---
+++ server.js
@@ -0,0 +1,18 @@
+const { app } = require('./app');
+const { db } = require('./utils/database.util');
+
+//models
+
+//utils
+
+db.authenticate()
+  .then(() => console.log('db authenticate'))
+  .catch((err) => console.log(err));
+
+db.sync()
+  .then(() => console.log('db sync'))
+  .catch((err) => console.log(err));
+
+app.listen(4000, () => {
+  console.log('express running');
+});
